f0f5e4ef05843ef27fa910bdf11c3048e1285a8c
Krzysztof Kubicki <kkubicki@jitsolutions.pl>
2018-10-31 20:40:09 +0100

    Login VC work in progress

---
+++ TestRun2/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  TestRun2
+//
+//  Created by Krzysztof kubicki on 31/10/2018.
+//  Copyright © 2018 Kubicki Engineering. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+
+let TO_LOGIN = "toLogin"
