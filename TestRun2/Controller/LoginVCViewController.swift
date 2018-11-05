8af41996b1eda7872e34837a72a54a83d9c1882c
Krzysztof Kubicki <kkubicki@jitsolutions.pl>
2018-11-05 13:29:40 +0100

    Login Screen with dismiss screen button

---
+++ TestRun2/Controller/LoginVCViewController.swift
@@ -0,0 +1,22 @@
+//
+//  LoginVCViewController.swift
+//  TestRun2
+//
+//  Created by Krzysztof kubicki on 05/11/2018.
+//  Copyright © 2018 Kubicki Engineering. All rights reserved.
+//
+
+import UIKit
+
+class LoginVCViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
