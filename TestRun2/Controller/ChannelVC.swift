41a6d0c8ace3a36a3bf2ce392aea632d096b6403
Krzysztof Kubicki <kkubicki@jitsolutions.pl>
2018-11-08 10:39:22 +0100

    Added create account screen with shitty layout

--- TestRun2/Controller/ChannelVC.swift
+++ TestRun2/Controller/ChannelVC.swift
@@ -11,12 +11,17 @@ import UIKit
 class ChannelVC: UIViewController {
 
     @IBOutlet weak var loginBtn: UIButton!
-    @IBAction func loginBtnPressed(_ sender: Any) {
-        performSegue(withIdentifier: TO_LOGIN, sender: nil)
+    @IBAction func prepareForUnwind(segue: UIStoryboardSegue) {
+        
     }
+  
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
+    }
+    
+    @IBAction func loginBtnPressed(_ sender: Any) {
+        performSegue(withIdentifier: TO_LOGIN, sender: nil)
     }
 }
--- TestRun2/Utilities/Constants.swift
+++ TestRun2/Utilities/Constants.swift
@@ -12,3 +12,4 @@ import Foundation
 
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
+let UNWIND = "unwindToChannel"
